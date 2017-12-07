094d387031d470ba86b281b73b18e3cf9bed13ac
Justin Ware <justin@warebrained.com>
2017-12-07 09:55:37 +0000

    Initial structure and commit

---
+++ test/configSpec.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import { the, should } from 'basis-testing';
+
+the('config', () => {
+    
+  should('do things', () => {
+
+    expect(true).to.equal(true);
+  });
+});
